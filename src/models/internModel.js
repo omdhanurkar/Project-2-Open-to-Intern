566210744e1c8829c5aa86e997b8abab7050159b
Omprakash <omdhanurkar444@gmail.com>
2022-09-14 11:33:26 +0530

    models

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -1,1 +1,38 @@
 const mongoose = require('mongoose');
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const internSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+
+    },
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    collegeId: {
+        type: ObjectId,
+        ref: "College"
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+
+    }
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Intern", internSchema);
